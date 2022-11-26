b10648ec66398630183f9ff608162c2e346a2bd3
Martin Alcock <martin.alcock@forgerock.com>
2022-11-26 23:35:35 +0000

    hmmmm

--- lua/telescope/_extensions/codefresh_telescope.lua
+++ lua/telescope/_extensions/codefresh_telescope.lua
@@ -1,7 +1,5 @@
-local codefresh_telescope = require 'telescope._extensions.codefresh_telescope'
-
-return require 'telescope'.register_extension {
+return require("telescope").register_extension {
   exports = {
-    codefresh_telescope = codefresh_telescope.builds
+    builds = require("codefresh_telescope").builds
   },
 }
