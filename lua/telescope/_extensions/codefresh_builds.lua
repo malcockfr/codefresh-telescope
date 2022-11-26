941a3a5cd6d36f30c09d4ae4684912c01db9eb70
Martin Alcock <martin.alcock@forgerock.com>
2022-11-26 22:37:21 +0000

    refactor

--- lua/telescope/_extensions/codefresh_builds.lua
+++ lua/telescope/_extensions/codefresh_builds.lua
@@ -4,13 +4,10 @@ if not ok then
   error "it's broken'"
 end
 
-local get_for_current_branch = require 'telescope._extensions.codefresh_builds.get_for_current_branch'
-
-local default_opts = {}
-local opts = default_opts
-
 return telescope.register_extension {
+  setup = function(ext_config, config)
+  end,
   exports = {
-    get_for_current_branch = function(_) get_for_current_branch(opts) end,
+    codefresh_builds = require("telescope._extensions.codefresh_builds").codefresh_builds
   },
 }
